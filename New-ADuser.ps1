983c7194f3dfa6a3bface0f3b7c06eb9fe0476f4
FlyBoyM <31342102+FlyBoyM@users.noreply.github.com>
2017-08-26 06:41:36 -0400

    Add files via upload

---
+++ New-ADuser.ps1
@@ -0,0 +1,28 @@
+﻿Import-Module ActiveDirectory
+
+Get-ADOrganizationalUnit -Filter 'Name -like "*"' | FT Name, DistinguishedName -A 
+
+$Name = Read-Host "Please Enter User Name"
+$Path = Read-Host "Please Enter Path"
+$Group = Read-Host "Please Enter Group Name"
+
+New-ADUser `
+ -Name $Name `
+ -Path $Path `
+ -SamAccountName $Name `
+ -DisplayName $Name `
+ -AccountPassword (ConvertTo-SecureString "MyP@ssword123" -AsPlainText -Force) `
+ -ChangePasswordAtLogon $true  `
+ -Enabled $true
+Add-ADGroupMember $Group $Name
+
+If ($Name) { 
+    $Name = $Name.ToUpper().Trim() 
+    $Res = (Get-ADPrincipalGroupMembership $Name | Measure-Object).Count 
+    If ($Res -GT 0) { 
+        Write-Output "`n" 
+        Write-Output "The User $Name Is A Member Of The Following Groups:" 
+        Write-Output "===========================================================" 
+        Get-ADPrincipalGroupMembership $Name | Select-Object -Property Name, GroupScope, GroupCategory | Sort-Object -Property Name | FT -A 
+    } 
+}
